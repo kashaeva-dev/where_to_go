0eb1f25a2c74900dedad36c8f6be74033464ced9
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-14 21:07:06 +0300

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -132,11 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = env('STATIC_URL', 'static/')
-STATIC_ROOT = env('STATIC_ROOT', os.path.join(BASE_DIR, "static"))
-
-MEDIA_ROOT = env('MEDIA_ROOT', os.path.join(BASE_DIR,'media'))
-MEDIA_URL = '/media/'
+if DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_URL = 'static/'
+else:
+    MEDIA_ROOT = '/home/kashseva/where_to_go/media'
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = '/home/kashseva/where_to_go/static'
+    STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
