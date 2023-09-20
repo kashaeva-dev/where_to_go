31641dca5509571c440b8956b61b78c0572e56c2
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-20 19:49:12 +0300

    Change media and static settings

--- conf/settings.py
+++ conf/settings.py
@@ -133,16 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-    STATIC_URL = 'static/'
-else:
-    MEDIA_ROOT = '/home/kashseva/where_to_go/media'
-    MEDIA_URL = '/media/'
-    STATIC_ROOT = '/home/kashseva/where_to_go/static'
-    STATIC_URL = '/static/'
+MEDIA_ROOT = env.path('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+MEDIA_URL = env('MEDIA_URL', '/media/')
+
+STATIC_ROOT = env.path('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+STATIC_URL = env('STATIC_URL', 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
